809069e1b79ebf3d5a7576be0c9a235be8d9c04a
Freddy <ga83qix@mytum.de>
2023-01-24 16:50:28 -0800

    Added info in the comments

--- measure_sgnl_pwr_and_incr_theta_null.sh
+++ measure_sgnl_pwr_and_incr_theta_null.sh
@@ -1,11 +1,15 @@
+# This script is run on machine wcsng-24.
+# Run this SECOND
+# Use script "start_srs.sh" to start transmission from srsenb.
+
 # Start collecting samples at a USRP with srsRAN DL center frequency (2.68 GHz)
 # and 10 MHz BW (50 PRB) and advance theta_null. Python script will iterate 
 # over all angles 1° apart at once per second frequency. Wait 1 second (10 M samples)
 # before iterating theta_null.
-# Use script "start_srs.sh" to start transmission from srsenb.
 
 # Start recording samples
 uhd_rx_cfile -f 2680M -r 10M -N 3610M pwr_measure_all_angles &
-sleep 1
+# Wait long enough for USRP to initialize
+sleep 4
 # Start python script to increment over angles of theta_null
 python3 set_theta_null.py
--- start_srs.sh
+++ start_srs.sh
@@ -1,8 +1,11 @@
+# This script is run on machine wcsng-23.
+# Run this FIRST
+# Use script "measure_sgnl_pwr_and_incr_theta_null.sh" to start recording samples
+# and advancing theta_null and iterate over a set of angles to measure.
+
 # Start srsepc and srsenb. 
 # srsenb will read theta_null to calculate pre-coding weight p2.
 # theta_null is the angle at which we want to create a null/low signal power.
-# Use script "measure_sgnl_pwr_and_incr_theta_null.sh" to start recording samples
-# and advancing theta_null and iterate over a set of angles to measure.
 
 # Set first angle to 0.
 echo "0" > srsRAN/lib/src/phy/mimo/theta_null.txt &
@@ -10,4 +13,5 @@ echo "0" > srsRAN/lib/src/phy/mimo/theta_null.txt &
 sudo srsRAN/build/srsepc/src/srsepc &
 sleep 2
 sudo srsRAN/build/srsenb/src/srsenb &
-sleep 3
+# Wait for srsenb to initialize
+sleep 6
