c05f3c48e7622347bbddc3e5f958fa3fac66a03c
Freddy <ga83qix@mytum.de>
2023-01-25 10:59:40 -0800

    Debugging theta_null and p2 calc in precoding function

--- srsRAN/lib/src/phy/mimo/precoding.c
+++ srsRAN/lib/src/phy/mimo/precoding.c
@@ -1953,11 +1953,13 @@ int srsran_precoding_diversity(cf_t* x[SRSRAN_MAX_LAYERS],
 {
   // Frederik
   int p1 = 1;
-  char theta_null_char = 'a';
+  char* file_content = (char*)malloc(3);
   int theta_null = 0;
   double theta_beam_rad;
   fptr = fopen("theta_null", "r");
-  theta_null_char = fgetc(fptr);
+  // theta_null_char = fgetc(fptr);
+  fgets(file_content,3,fptr);
+  puts(file_content);
   //printf("srsenb: Using theta_null: %d°\n", theta_null);
   // Calculate theta_beam_rad from theta_null
   if (sin(theta_null*M_PI/180) > 0) {
