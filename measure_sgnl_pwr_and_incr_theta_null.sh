1fc98473b0a098705e1fbfb9eb60be25d7c237dc
Freddy <ga83qix@mytum.de>
2023-01-26 15:27:21 -0800

    Adjusted number of samples collected with uhd_rx_cfile

--- measure_sgnl_pwr_and_incr_theta_null.sh
+++ measure_sgnl_pwr_and_incr_theta_null.sh
@@ -8,7 +8,8 @@
 # before iterating theta_null.
 
 # Start recording samples
-uhd_rx_cfile -f 2680M -r 10M -N 1900M pwr_measure_all_angles &
+# 1910M smaples: (10M samples per 1° angle)*181 angles + 10M*10 sec wait
+uhd_rx_cfile -f 2680M -r 10M -N 1910M pwr_measure_all_angles &
 # Wait long enough for USRP to initialize
 sleep 10
 # Start python script to increment over angles of theta_null
