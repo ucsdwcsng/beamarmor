ff6670286af9ef8cd69ab988c474e417c9ab601a
Frederik <f.zumegen@tum.de>
2023-02-20 15:03:28 -0800

    corrected name of iq sample file for antenna 1

--- gr_scripts/single_tone.py
+++ gr_scripts/single_tone.py
@@ -6,9 +6,9 @@
 #
 # GNU Radio Python Flow Graph
 # Title: Not titled yet
-# GNU Radio version: 3.8.2.0
+# GNU Radio version: v3.10.5.1-1-gf972571a
 
-from distutils.version import StrictVersion
+from packaging.version import Version as StrictVersion
 
 if __name__ == '__main__':
     import ctypes
@@ -24,6 +24,7 @@ from gnuradio import blocks
 import pmt
 from gnuradio import gr
 from gnuradio.filter import firdes
+from gnuradio.fft import window
 import sys
 import signal
 from PyQt5 import Qt
@@ -33,12 +34,14 @@ from gnuradio import eng_notation
 from gnuradio import uhd
 import time
 
+
+
 from gnuradio import qtgui
 
 class single_tone(gr.top_block, Qt.QWidget):
 
     def __init__(self):
-        gr.top_block.__init__(self, "Not titled yet")
+        gr.top_block.__init__(self, "Not titled yet", catch_exceptions=True)
         Qt.QWidget.__init__(self)
         self.setWindowTitle("Not titled yet")
         qtgui.util.check_set_qss()
@@ -77,6 +80,7 @@ class single_tone(gr.top_block, Qt.QWidget):
         ##################################################
         # Blocks
         ##################################################
+
         self.uhd_usrp_sink_0 = uhd.usrp_sink(
             ",".join(("", "")),
             uhd.stream_args(
@@ -87,19 +91,20 @@ class single_tone(gr.top_block, Qt.QWidget):
             '',
         )
         self.uhd_usrp_sink_0.set_subdev_spec('A:0 B:0', 0)
-        self.uhd_usrp_sink_0.set_center_freq(cent_freq, 0)
-        self.uhd_usrp_sink_0.set_gain(10, 0)
-        self.uhd_usrp_sink_0.set_antenna('TX/RX', 0)
-        self.uhd_usrp_sink_0.set_center_freq(cent_freq, 1)
-        self.uhd_usrp_sink_0.set_gain(10, 1)
-        self.uhd_usrp_sink_0.set_antenna('TX/RX', 1)
         self.uhd_usrp_sink_0.set_samp_rate(samp_rate)
-        self.uhd_usrp_sink_0.set_time_unknown_pps(uhd.time_spec())
+        self.uhd_usrp_sink_0.set_time_unknown_pps(uhd.time_spec(0))
+
+        self.uhd_usrp_sink_0.set_center_freq(cent_freq, 0)
+        self.uhd_usrp_sink_0.set_antenna('TX/RX', 0)
+        self.uhd_usrp_sink_0.set_gain(10, 0)
+
+        self.uhd_usrp_sink_0.set_center_freq(cent_freq, 1)
+        self.uhd_usrp_sink_0.set_antenna('TX/RX', 1)
+        self.uhd_usrp_sink_0.set_gain(10, 1)
         self.blocks_file_source_0_0 = blocks.file_source(gr.sizeof_gr_complex*1, '/tmp/tx_data2.iq', True, 0, 0)
         self.blocks_file_source_0_0.set_begin_tag(pmt.PMT_NIL)
-        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_gr_complex*1, '/tmp/tx_tone1.iq', True, 0, 0)
+        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_gr_complex*1, '/tmp/tx_data1.iq', True, 0, 0)
         self.blocks_file_source_0.set_begin_tag(pmt.PMT_NIL)
-
 
 
         ##################################################
@@ -112,6 +117,9 @@ class single_tone(gr.top_block, Qt.QWidget):
     def closeEvent(self, event):
         self.settings = Qt.QSettings("GNU Radio", "single_tone")
         self.settings.setValue("geometry", self.saveGeometry())
+        self.stop()
+        self.wait()
+
         event.accept()
 
     def get_samp_rate(self):
@@ -132,7 +140,6 @@ class single_tone(gr.top_block, Qt.QWidget):
 
 
 
-
 def main(top_block_cls=single_tone, options=None):
 
     if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
@@ -147,6 +154,9 @@ def main(top_block_cls=single_tone, options=None):
     tb.show()
 
     def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+
         Qt.QApplication.quit()
 
     signal.signal(signal.SIGINT, sig_handler)
@@ -156,11 +166,6 @@ def main(top_block_cls=single_tone, options=None):
     timer.start(500)
     timer.timeout.connect(lambda: None)
 
-    def quitting():
-        tb.stop()
-        tb.wait()
-
-    qapp.aboutToQuit.connect(quitting)
     qapp.exec_()
 
 if __name__ == '__main__':
